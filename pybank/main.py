3185af8c88574a9db4af4d971cbb3b1c3afb0034
Rob <robchess66@gmail.com>
2019-07-30 16:54:32 -0500

    working, no comments

--- pybank/main.py
+++ pybank/main.py
@@ -8,8 +8,10 @@ previous_date = 0
 current_profit = 0
 current_date = 0
 average_change = 0
-greatest_increase = 0
-greatest_decrease = 0
+least_profit = 0
+least_profit_date = ""
+highest_profit = 0
+highest_profit_date = ""
 
 budget_csv = os.path.join("./Resources", "budget_data_test.csv")
 
@@ -19,10 +21,16 @@ with open(budget_csv, newline="") as csv_file:
     # loop through file
     for row in csv_reader:
         count_rows = count_rows + 1
-        print(row)
+        # print(row)
         sum_rows = sum_rows + int(row[1])
         current_profit = int(row[1])
         current_date = row[0]
+        if current_profit > highest_profit:
+            highest_profit = current_profit
+            highest_profit_date = current_date
+        if current_profit < least_profit:
+            least_profit = current_profit
+            least_profit_date = current_date
         if count_rows > 1:
            average_change = average_change + (current_profit - previous_profit)
            previous_profit = current_profit
@@ -34,11 +42,13 @@ with open(budget_csv, newline="") as csv_file:
 
 
 print("  ")
-print("Finasncial Analysis")
+print("Finacial Analysis")
 print("------------------------------")
 print(f"Total Monthss: {count_rows}")
 print(f"Total: ${sum_rows}")
 average_change = average_change / (count_rows -1)
 print(f"Average Change: ${round(average_change, 2)}")
+print(f"Greatist Increase in Profits: {highest_profit_date}  (${highest_profit})")
+print(f"Greatist Decrease in Profits: {least_profit_date}  (${least_profit})")
 
   
