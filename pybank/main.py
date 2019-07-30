9730fd45b99f772458ca6874ae912af6e428d7d1
Rob <robchess66@gmail.com>
2019-07-30 00:06:08 -0500

    bedtime

--- pybank/main.py
+++ pybank/main.py
@@ -1,6 +1,16 @@
 import os
 import csv
 
+count_rows = 0
+sum_rows = 0
+previous_profit
+previous_date
+current_profit
+current_date
+average_change = 0
+greatest_increase = 0
+greatest_decrease = 0
+
 budget_csv = os.path.join("./Resources", "budget_data.csv")
 
 with open(budget_csv, newline="") as csv_file:
@@ -8,15 +18,24 @@ with open(budget_csv, newline="") as csv_file:
     header = next(csv_reader)
     # loop through file
     for row in csv_reader:
-        print(row)
-        # if current row = user input
-        # if float(row[7]) >= 5:
-        #     if row == 1:
-        #         print(f"Cereal: {header[0]} Fiber: {header[7]}")
-        #     print(f"Cereal: {row[0]} Fiber: {row[7]}")
-        #     found = True
-        # else keep searching
-    # if nothing found print not found
-# if (found == False):
-#     print("No Cereals found")
+        count_rows = count_rows + 1
+        sum_rows = sum_rows + int(row[1])
+        current_profit = int(row[1])
+        current_date = row[0]
+        if count_rows > 1:
+           average_change = average_change + (current_profit - previous_profit)
+           previous_profit = current_profit
+           previous_date = current_date
+        else:
+           previous_profit = current_profit
+           previous_date = current_date
+            
 
+print("Financial Analysis")
+print("------------------------------")
+print(f"Total Months: {count_rows}")
+print(f"Total: ${sum_rows}")
+average_change = average_change / total_months
+print(f"Average Change: ${average_change}")
+
+  
