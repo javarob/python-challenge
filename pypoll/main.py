b2ba5c8c3372e8f6f2e778468451cb55377fdd1a
Rob <robchess66@gmail.com>
2019-08-01 23:17:07 -0500

    all the stuff, complete

--- pypoll/main.py
+++ pypoll/main.py
@@ -18,14 +18,14 @@ with open(budget_csv, newline="") as csv_file:
     for row in csv_reader:  # grab each line in csv
         vote_total = vote_total + 1 # get total number of records
         
-        candidate = row[2]
+        candidate = row[2] # grab candidate by name
         for x in range(len(votes_by_candidate)):  # for each candidate (key) in dict 
             if candidate in votes_by_candidate:   # if candidate is listed in dict
                 exists = True
                 votes_by_candidate[candidate] = votes_by_candidate[candidate] + 1 # add to vote cnt
                 break
             else:
-                exists = False # key not in dict
+                exists = False # key not already in dict
 
         if exists == False:  # key wasn't in dict so add & add vote cnt too
             votes_by_candidate[candidate] = 1
@@ -42,3 +42,4 @@ for key,val in votes_by_candidate.items(): # list candidates and votes
     print (f"{key}:  {round(val/vote_total*100, 3)}% ({val}) ")
 print("------------------------------")
 print(f"Winner: {winner}")
+print("------------------------------")
