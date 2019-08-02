eefbbdf299442829252a81f7c4e78d6b9b411ac9
Rob <robchess66@gmail.com>
2019-08-01 22:53:23 -0500

    logic complete plus comments

--- pypoll/main.py
+++ pypoll/main.py
@@ -1,14 +1,13 @@
 import os
 import csv
 
-vcount = 0
 candidate = "" 
-candidates = []
 exists = False
 vote_total = 0
 candidate_vote_count = 0
 votes_by_candidate ={}
-total_val = 0 # temp for tsting
+prior = 0
+winner = ""
 
 budget_csv = os.path.join("./Resources", "election_data.csv")
 
@@ -16,30 +15,32 @@ with open(budget_csv, newline="") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     header = next(csv_reader)
     # loop through file by line
-    for row in csv_reader:
+    for row in csv_reader:  # grab each line in csv
         vote_total = vote_total + 1 # get total number of records
         
         candidate = row[2]
-        for x in range(len(votes_by_candidate)):
-            if candidate in votes_by_candidate:
+        for x in range(len(votes_by_candidate)):  # for each candidate (key) in dict 
+            if candidate in votes_by_candidate:   # if candidate is listed in dict
                 exists = True
-                votes_by_candidate[candidate] = votes_by_candidate[candidate] + 1
+                votes_by_candidate[candidate] = votes_by_candidate[candidate] + 1 # add to vote cnt
                 break
             else:
-                exists = False
+                exists = False # key not in dict
 
-        if exists == False:
+        if exists == False:  # key wasn't in dict so add & add vote cnt too
             votes_by_candidate[candidate] = 1
 
-for key,val in votes_by_candidate.items():
-    total_val = total_val + val
-    print (key," => ",val)
-
+for key,val in votes_by_candidate.items(): # list candidates and votes
+    if val > prior: # find highest votes
+        prior = val
+        winner = key
+    print (f"{key}:  {round(val/vote_total*100, 3)} ({val}) ")
 
 # print("  ")
 # print(find_candidates)
 print(f"row count: {vote_total}")
-print(f"votes total {total_val}")
+print(f"Winner: {winner}")
+
         
 
            
