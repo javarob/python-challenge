ffe6af4390774386e4ef802c01944186784537c3
Rob <robchess66@gmail.com>
2019-08-01 21:26:04 -0500

    poll - gettin' evn closer

--- pypoll/main.py
+++ pypoll/main.py
@@ -8,6 +8,7 @@ exists = False
 vote_total = 0
 candidate_vote_count = 0
 votes_by_candidate ={}
+total_val = 0 # temp for tsting
 
 budget_csv = os.path.join("./Resources", "election_data.csv")
 
@@ -17,22 +18,22 @@ with open(budget_csv, newline="") as csv_file:
     # loop through file by line
     for row in csv_reader:
         vote_total = vote_total + 1 # get total number of records
-        #print(row)
         
         candidate = row[2]
         for x in range(len(candidates)):
             if candidate == candidates[x]:
                 exists = True
                 vcount = vcount + 1
-                votes_by_candidate[candidate] = vcount
+                votes_by_candidate[candidate] = votes_by_candidate[candidate] + 1
+                #print(f"votes: {votes_by_candidate[candidate]}")
                 break
             else:
                 exists = False
 
         if exists == False:
-            #candidates.append(candidate)
+            candidates.append(candidate)
             vcount = vcount + 1
-            votes_by_candidate[candidate] = vcount + 1
+            votes_by_candidate[candidate] = 1
 
     # for candidate in candidates:
     #     print(candidate)
